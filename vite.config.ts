d29462afb50cd78714ae27303575af555d214250
zaidalp449l <155318609+zaidalp449l@users.noreply.github.com>
2025-10-27 22:23:55 +0530

    Add base path for deployment
    Set base path for deployment in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Peoples-republic-of-electronics/", // ✅ Add this line
+
   plugins: [
     react(),
     // The code below enables dev tools like taking screenshots of your site
